0e7037afd08166039cb626bdd1298c8637d309cc
chemars <37469933+chemars@users.noreply.github.com>
2020-01-09 18:34:57 +0800

    Update plot.sh

--- plot.sh
+++ plot.sh
@@ -3,14 +3,15 @@ export GMT_SESSION_NAME=$$	# Set a unique session name
 
 data=intensity.csv
 info=information.csv
+ts=12p
 
 gmt begin plot png A+m1c
 gmt basemap -Jm3c -R119/123/21/26 -Ba1 -B+t"Intensity Map"
 gmt coast -Df -W0.3p
-gawk 'BEGIN {FS=","}; {if ($5 == 0 || $5 == 1) print $3, $4, $5}' $data | gmt text -F+f12p,,black+jMC
-gawk 'BEGIN {FS=","}; {if ($5 == 2) print $3, $4, $5}' $data | gmt text -F+f12p,,blue+jMC
-gawk 'BEGIN {FS=","}; {if ($5 == 3 || $5 == 4) print $3, $4, $5}' $data | gmt text -F+f12p,,0/128/0+jMC
-gawk 'BEGIN {FS=","}; {if (NR > 1 && $5 >= 5) print $3, $4, $5}' $data | gmt text -F+f12p,,red+jMC
+gawk 'BEGIN {FS=","}; {if ($5 == 0 || $5 == 1) print $3, $4, $5}' $data | gmt text -F+f$ts,,black+jMC
+gawk 'BEGIN {FS=","}; {if ($5 == 2) print $3, $4, $5}' $data | gmt text -F+f$ts,,blue+jMC
+gawk 'BEGIN {FS=","}; {if ($5 == 3 || $5 == 4) print $3, $4, $5}' $data | gmt text -F+f$ts,,0/128/0+jMC
+gawk 'BEGIN {FS=","}; {if (NR > 1 && $5 >= 5) print $3, $4, $5}' $data | gmt text -F+f$ts,,red+jMC
 gawk 'BEGIN {FS=","}; {if (NR > 1) print $2, $3}' $info | gmt plot -Gred -Sa0.5c
 gawk 'BEGIN {FS=","}; {if (NR > 1) print 119.2, 21.30, "Origin time: "$1}' $info | gmt text -F+f10p,,black+jML
 gawk 'BEGIN {FS=","}; {if (NR > 1) print 119.2, 21.15, "Magnitude: M@-L@-"$5"  Depth: "$4" km"}' $info | gmt text -F+f10p,,black+jML
