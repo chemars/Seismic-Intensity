aeddf05e236f77b110d0c8b83faab834b37243ea
chemars <37469933+chemars@users.noreply.github.com>
2020-02-15 13:17:58 +0800

    Create auto.m

---
+++ auto.m
@@ -0,0 +1,18 @@
+clear all; close all; fclose all; clc;
+url = "https://ci.taiwan.gov.tw/dsp/history/eq_cwb/";
+
+str = urlread(url);
+zipstr = regexp(str,'\w*.zip','match');
+filename = zipstr{end}
+urlwrite([url filename],filename);
+unzip(filename)
+
+tmp = dir([pwd "\\2*\\*.??t"]);
+for ii = 1:size(tmp,1)
+  movefile([tmp(ii).folder "\\" tmp(ii).name],"data");
+endfor
+delete *.zip *.dat *.xml
+rmdir(tmp(1).folder);
+
+intensity
+information
