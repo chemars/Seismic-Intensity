d5d81357e0954c9cd2c143969e56bbfede5c4325
chemars <37469933+chemars@users.noreply.github.com>
2020-08-24 09:35:06 +0800

    Update tool_cwb_parameters.m

--- tool_cwb_parameters.m
+++ tool_cwb_parameters.m
@@ -1,8 +1,8 @@
 clear all; close all; fclose all; clc;
-filename = "2020046.txt";
-output = "information.csv";
+filename = "2020046";
+output = [filename "_new.csv"];
 
-fileID = fopen(filename,"r");
+fileID = fopen([filename ".txt"],"r");
 rawdata = textscan(fileID,["%s%s%s%s" repmat("%s%f",[1,10]) "%s%s%s%f%s%f"],"Delimiter",{",","="},"HeaderLines",5);
 station_code = rawdata{2};
 station_name = rawdata{4};
@@ -14,7 +14,7 @@ pgv = rawdata{30};
 fclose(fileID);
 
 [nrows,ncols] = size(station_code);
-fid = fopen(output,"a");
+fid = fopen(output,"w");
 formatSpec = "%s,%s,%.2f,%.2f,%s,%.2f,%.2f\n";
 for row = 1:nrows-1
     fprintf(fid,formatSpec,station_code{row,1},station_name{row,1},station_lon(row,1),station_lat(row,1),I_new{row,1},pga(row,1),pgv(row,1));
