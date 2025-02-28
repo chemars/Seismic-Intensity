da3de7b415eb0f11b8d746c038f529e54a59257c
chemars <37469933+chemars@users.noreply.github.com>
2025-02-28 11:32:38 +0800

    Update tool_cwa_web.m

--- tool_cwa_web.m
+++ tool_cwa_web.m
@@ -1,10 +1,12 @@
 clear all; close all; fclose all; clc;
 url = "https://www.cwa.gov.tw/V8/C/E/MOD/EQ_ROW.html";
-urlcwb = "https://www.cwa.gov.tw";
+urlcwa = "https://www.cwa.gov.tw";
 
 str = urlread(url);
-urlstr = regexp(str,'href="([\w-/]*\.html)"','tokens');
-str2 = urlread([urlcwb char(urlstr{1})]);
-txtstr = regexp(str2,'title="\W+\s(\w*\.txt)"','tokens');
-urlstr2 = regexp(str2,'href="([\w-/]*\.txt[\w\?=]*)"','tokens');
-urlwrite([urlcwb char(urlstr2{1})],char(txtstr{1}));
+urlstr = regexp(str,'href="([\w/-]*\.html)"','tokens');
+
+str2 = urlread([urlcwa char(urlstr{1})]);
+urltxt = regexp(str2, 'href="([^"]+\.txt(?:\?[^"]*)?)"', 'tokens');
+urlname = regexp(str2, 'href="[^"]*/([^/"]+\.txt)(?:\?[^"]*)?"', 'tokens');
+
+urlwrite([urlcwa char(urltxt{1})],char(urlname{1}));
