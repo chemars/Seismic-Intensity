67d29f9a2f504f21bc8e9442748c247097ba9dd0
chemars <37469933+chemars@users.noreply.github.com>
2026-01-01 08:53:21 +0800

    Update tool_auto.m

--- tool_auto.m
+++ tool_auto.m
@@ -1,14 +1,14 @@
 clear all; close all; fclose all; clc;
 authorization = "";
 if isempty(authorization)
-    error("Require Authorization. See https://opendata.cwa.gov.tw/about/application/general")
+    error("Require Authorization Key. See https://opendata.cwa.gov.tw/about/application/general")
 endif
 
 json_name = "E-A0015-004.json";
 urlwrite(["https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/E-A0015-004?Authorization=" authorization "&downloadType=WEB&format=JSON"],json_name);
 jsonStr = fileread(json_name);
 jsonData = jsondecode(jsonStr);
-file_name = [jsonData.cwaopendata.Dataset.Resource.ResourceDesc "." jsonData.cwaopendata.Dataset.Resource.MimeType]
+file_name = [jsonData.cwaopendata.Dataset.Resource.ResourceDesc ".zip"]
 if isfile(file_name)
     error("Already Download")
 endif
@@ -22,7 +22,7 @@ endfor
 rmdir(tmp(1).folder);
 
 eqyr = jsonData.cwaopendata.Dataset.Resource.ResourceDesc(1:4);
-eqnum = jsonData.cwaopendata.Dataset.Resource.ResourceDesc(1:7)
+eqnum = jsonData.cwaopendata.Dataset.Resource.ResourceDesc(1:7);
 urlwrite(["https://scweb.cwa.gov.tw/zh-tw/earthquake/download?file=%2FdrawTrace%2Foutcome%2F" eqyr "%2F" eqnum ".txt"],[eqnum ".txt"]);
 
 tool_cwa_parameters
